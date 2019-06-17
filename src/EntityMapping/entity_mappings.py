aa032f3de99e96015542d68e326895812f269a06
Julian Weise <privat@julianweise.de>
2019-06-17 16:02:33 +0200

    Add entity mapping parsing

---
+++ src/EntityMapping/entity_mappings.py
@@ -0,0 +1,17 @@
+import time
+
+
+class EntityMappings:
+
+    def __init__(self):
+        self.imported = time.time()
+        self._entity_mappings = {}
+
+    def add(self, knowledgebase_id, embedding_tag):
+        self._entity_mappings[knowledgebase_id] = embedding_tag
+
+    def __getitem__(self, item):
+        return self._entity_mappings[item]
+
+    def __contains__(self, item):
+        return item in self._entity_mappings
--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 import argparse
 from pathlib import Path
 
+from src.EntityMappingConfiguration.entity_mappings_factory import EntityMappingsFactory
 from src.TaskConfiguration import TaskCategoryCollectionFactory
 
 
 def main(args):
     test_configuration = TaskCategoryCollectionFactory.create_categories_from_file(args.test_set_config)
+    entity_mappings = EntityMappingsFactory.create_from_configuration_file(args.entity_mapping)
 
 
 if __name__ == "__main__":
@@ -14,6 +16,12 @@ if __name__ == "__main__":
         "--test-set-config",
         type=Path,
         help="Path to the test set configuration file.",
+        required=True
+    )
+    parser.add_argument(
+        "--entity-mapping",
+        type=Path,
+        help="Path to the csv file containing knowledgebase to embedding tags entity mappings.",
         required=True
     )
     main(parser.parse_args())

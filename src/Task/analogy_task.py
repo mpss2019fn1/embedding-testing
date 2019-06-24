cfca9959c035d04c5972a5c4d42384723fb24eea
Johannes Schneider <johannes.schneider@student.hpi.de>
2019-06-24 14:51:05 +0200

    implementation and tests for analogy task

--- src/Task/analogy_task.py
+++ src/Task/analogy_task.py
@@ -1,10 +1,8 @@
+from src.Result.case_result import CaseResult
 from src.Task import AbstractTask
 
 
 class AnalogyTask(AbstractTask):
-
-    def _run(self):
-        pass
 
     @classmethod
     def configuration_identifier(cls):
@@ -15,5 +13,27 @@ class AnalogyTask(AbstractTask):
         from src.Task import TaskType
         return TaskType.ANALOGY
 
-    def run(self):
-        pass
+    def _run(self):
+        embedding = self._test_configuration.embedding
+        linking = self._test_configuration.entity_linkings
+        for indexA, lineA in enumerate(self._test_set_lines()):
+            for indexB, lineB in enumerate(self._test_set_lines()):
+                if indexA >= indexB:
+                    continue
+
+                entity_a1 = linking[lineA[0]]
+                entity_a2 = linking[lineA[1]]
+                vector_a1 = embedding[entity_a1]
+                vector_a2 = embedding[entity_a2]
+                vector1 = vector_a2 - vector_a1
+
+                entity_b1 = linking[lineB[0]]
+                entity_b2 = linking[lineB[1]]
+                vector_b1 = embedding[entity_b1]
+                vector_b2 = embedding[entity_b2]
+                vector2 = vector_b2 - vector_b1
+
+                result = self.metric.compute(vector1, vector2)
+                is_similar = self.metric.is_better_than_noise(result, embedding)
+
+                yield CaseResult([(entity_a1, entity_a2), (entity_b1, entity_b2)], True, result, is_similar)
--- test/Task/test_analogy_task.py
+++ test/Task/test_analogy_task.py
@@ -3,17 +3,17 @@ from pathlib import Path
 from src.Embedding.embedding_factory import EmbeddingFactory
 from src.EntityLinking.entity_linkings_factory import EntityLinkingsFactory
 from src.Metric.cosine_similarity import CosineSimilarity
-from src.Task import SimilarityTask
+from src.Task import AnalogyTask
 from src.TaskConfiguration import TaskConfigurationFactory
 from src.TestConfiguration.test_configuration import TestConfiguration
 from test.base_test_case import BaseTestCase
 
 
-class TestSimilarityTask(BaseTestCase):
+class TestAnalogyTask(BaseTestCase):
 
     def setup_method(self):
-        super(TestSimilarityTask, self).setup_method()
-        self.resource_directory = Path(self.resource_directory.absolute(), "similarity_task")
+        super(TestAnalogyTask, self).setup_method()
+        self.resource_directory = Path(self.resource_directory.absolute(), "analogy_task")
         self._test_configuration = self._create_test_configuration()
 
     def _create_test_configuration(self):
@@ -24,18 +24,17 @@ class TestSimilarityTask(BaseTestCase):
         return TestConfiguration(embedding, entity_linking, [], task_configs)
 
     def test_run(self):
-        task = SimilarityTask("Politicians", Path(self.resource_directory, "politicians.csv"), CosineSimilarity())
+        task = AnalogyTask("Capitals", Path(self.resource_directory, "capitals.csv"), CosineSimilarity())
         result = task.run(self._test_configuration)
 
-        assert 9 == len(result.case_results)
+        assert 3 == len(result.case_results)
         assert 100 == result.pass_rate()
         assert 0 < result.execution_duration()
 
     def test_run_with_failing_tests(self):
-        task = SimilarityTask("Politicians", Path(self.resource_directory, "politicians_with_failures.csv"),
-                              CosineSimilarity())
+        task = AnalogyTask("Capitals", Path(self.resource_directory, "capitals_with_failures.csv"), CosineSimilarity())
         result = task.run(self._test_configuration)
 
-        assert 9 == len(result.case_results)
-        assert (7 / 9) * 100 == result.pass_rate()
+        assert 3 == len(result.case_results)
+        assert (1 / 3) * 100 == result.pass_rate()
         assert 0 < result.execution_duration()

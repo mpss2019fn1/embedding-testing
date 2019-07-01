d2b816af1b640510a0dea5399331bdeff6ed95f6
Johannes Schneider <johannes.schneider@student.hpi.de>
2019-07-01 13:47:56 +0200

    add more tests for task file parser

--- test/FileParsing/ConfigurationFileParsing/test_task_file_parser.py
+++ test/FileParsing/ConfigurationFileParsing/test_task_file_parser.py
@@ -9,7 +9,7 @@ from test.base_test_case import BaseTestCase
 
 class TestTaskFileParser(BaseTestCase):
 
-    def test_create_task_from_configuration_with_absolute_paths(self):
+    def test_absolute_paths(self):
         config = {
             "name": "task-name",
             "type": "analogy",
@@ -21,7 +21,7 @@ class TestTaskFileParser(BaseTestCase):
         assert task.__class__ == AnalogyTask
         assert task.test_set == self.empty_file
 
-    def test_create_task_from_configuration_with_relative_paths(self):
+    def test_relative_paths(self):
         config = {
             "name": "task-name",
             "type": "analogy",
@@ -33,11 +33,48 @@ class TestTaskFileParser(BaseTestCase):
         assert task.__class__ == AnalogyTask
         assert task.test_set == self.empty_file
 
-    def test_create_task_from_configuration_test_set_missing(self):
+    def test_test_set_does_not_exist(self):
         config = {
             "name": "task-name",
             "type": "analogy",
             "test_set": f"./{uuid.uuid4()}"
+        }
+        task_file_parser = TaskFileParser(self.empty_file)
+        with pytest.raises(KeyError):
+            task = task_file_parser.create_task_from_configuration(config)
+
+    def test_name_is_missing(self):
+        config = {
+            "type": "analogy",
+            "test_set": str(self.empty_file.relative_to(self.test_dir))
+        }
+        task_file_parser = TaskFileParser(self.empty_file)
+        with pytest.raises(KeyError):
+            task = task_file_parser.create_task_from_configuration(config)
+
+    def test_name_is_empty(self):
+        config = {
+            "name": "",
+            "type": "analogy",
+            "test_set": str(self.empty_file.relative_to(self.test_dir))
+        }
+        task_file_parser = TaskFileParser(self.empty_file)
+        with pytest.raises(KeyError):
+            task = task_file_parser.create_task_from_configuration(config)
+
+    def test_test_set_missing(self):
+        config = {
+            "name": "task-name",
+            "type": "analogy",
+        }
+        task_file_parser = TaskFileParser(self.empty_file)
+        with pytest.raises(KeyError):
+            task = task_file_parser.create_task_from_configuration(config)
+
+    def test_type_missing(self):
+        config = {
+            "name": "",
+            "test_set": str(self.empty_file.relative_to(self.test_dir))
         }
         task_file_parser = TaskFileParser(self.empty_file)
         with pytest.raises(KeyError):

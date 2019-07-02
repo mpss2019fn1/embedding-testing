ccef59c32ecf051e759939e66be306513f311863
Johannes Schneider <johannes.schneider@student.hpi.de>
2019-07-02 10:28:04 +0200

    Feature/category result (#13)
    implementation and tests for test executor and test category result

--- test/Result/test_case_result.py
+++ test/Result/test_case_result.py
@@ -4,7 +4,6 @@ from src.Result.case_result import CaseResult
 class TestCaseResult:
 
     def test_success_representation(self):
-
         test_result = CaseResult("Angela_Merkel", "AKK", "AKK", True)
 
         assert str(test_result).startswith(CaseResult.PASSED_PREFIX)
@@ -12,7 +11,6 @@ class TestCaseResult:
         assert CaseResult.EXPECTED_OUTPUT_PREFIX not in str(test_result)
 
     def test_failure_representation(self):
-
         test_result = CaseResult("Angela_Merkel", "AKK", "Donald Trump", False)
 
         assert str(test_result).startswith(CaseResult.NOT_PASSED_PREFIX)
--- test/Result/test_task_result.py
+++ test/Result/test_task_result.py
@@ -21,8 +21,9 @@ class TestTaskResult:
 
     def test_has_results(self):
         task_result = TestTaskResult._create_enabled_task_result()
-        task_result.finalize()
+        assert task_result.has_results()
 
+        task_result.finalize()
         assert task_result.has_results()
 
     def test_has_execution_duration(self):
@@ -32,19 +33,15 @@ class TestTaskResult:
         task_result.finalize()
         assert task_result.execution_duration() > 0
 
-    def test_should_not_be_finalized_twice(self):
-        task_result = TestTaskResult._create_enabled_task_result()
-        task_result.finalize()
-
-        with pytest.raises(Exception):
-            task_result.finalize()
-
     def test_pass_rate(self):
         task_result = TestTaskResult._create_enabled_task_result()
+        assert task_result.pass_rate() == 0
+
         case_result = CaseResult("Vienna", "London", "Paris", False)
         task_result.add_case_result(case_result)
-        task_result.finalize()
+        assert task_result.pass_rate() == 0
 
+        task_result.finalize()
         assert task_result.pass_rate() == 50
 
     def test_representation_contains_all_case_results(self):
@@ -56,6 +53,14 @@ class TestTaskResult:
         assert len(str(task_result).split("\n")) == 3
         assert task_result.__str__() == task_result.__repr__()
 
+    def test_disabled_representation_starts_with_prefix(self):
+        task = CosineSimilarityTask("SimilarityTask", Path())
+        task_result = TaskResult(task, False)
+
+        assert not task_result.enabled
+        assert str(task_result).startswith(TaskResult.DISABLED_PREFIX)
+        assert len(str(task_result).split("\n")) == 1
+
     def test_empty_result(self):
         task = CosineSimilarityTask("SimilarityTask", Path())
         task_result = TaskResult(task, True)
@@ -66,3 +71,24 @@ class TestTaskResult:
         assert task_result.execution_duration() == 0
         assert len(str(task_result)) > 0
         assert len(str(task_result).split("\n")) == 1
+
+    def test_add_case_result_raises_if_finalized(self):
+        task = CosineSimilarityTask("SimilarityTask", Path())
+        task_result = TaskResult(task, True)
+        task_result.finalize()
+
+        with pytest.raises(Exception):
+            task_result.add_case_result(CaseResult("input", "expected", "actual", False))
+
+    def test_finalize_raises_if_finalized(self):
+        task = CosineSimilarityTask("SimilarityTask", Path())
+        task_result = TaskResult(task, True)
+        task_result.finalize()
+
+        with pytest.raises(Exception):
+            task_result.finalize()
+
+    def test_finalize_returns_self(self):
+        task_result = self._create_enabled_task_result()
+
+        assert task_result == task_result.finalize()

21d2930e437bd7cca066ee6306491e6ae14e66a4
Julian Weise <privat@julianweise.de>
2019-08-05 15:17:06 +0200

    Adapt test execution in order to utilize name resolution
    Co-authored-by: Johannes-Schneider <johannes.schneider@student.hpi.de>

---
+++ src/Testing/FileParsing/EntityLabelFileParsing/entity_label_file_parser.py
@@ -0,0 +1,30 @@
+import csv
+from pathlib import Path
+
+from src.Testing.EntityLabel.entity_labels import EntityLabels
+from src.Testing.FileParsing.abstract_file_parser import AbstractFileParser
+
+
+class EntityLabelFileParser(AbstractFileParser):
+    COLUMN_INDEX_KNOWLEDGEBASE_ID = 1
+    COLUMN_INDEX_EMBEDDING_LABEL = 0
+
+    @staticmethod
+    def create_from_file(configuration_file: Path):
+        if not configuration_file.exists():
+            return EntityLabels()
+
+        with configuration_file.open("r") as csv_stream:
+            csv_reader = csv.reader(csv_stream, delimiter=',')
+            entity_labels = EntityLabels()
+
+            next(csv_reader, None)  # skip header
+            for row in csv_reader:
+                if not row:
+                    continue
+
+                knowledgebase_id = row[EntityLabelFileParser.COLUMN_INDEX_KNOWLEDGEBASE_ID]
+                entity_label = row[EntityLabelFileParser.COLUMN_INDEX_EMBEDDING_LABEL]
+                entity_labels.add(knowledgebase_id, entity_label)
+
+            return entity_labels
